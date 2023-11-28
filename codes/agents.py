d610bc249c69fc8179237937e336da7c82c78aaf
alifthi <alifathi8008@gmail.com>
2023-11-28 14:21:16 -0600

    Federated_Deep_Q_Network: update_buffer and sellect_action methods developed.

--- codes/agents.py
+++ codes/agents.py
@@ -1,60 +1,71 @@
-import random
+import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import layers as ksl 
+from config import ENV,STATE_SIZE
+import gym 
 
 class agent:
-    def __init__(self,state_size,action_size,discount_factor=0.9,input_size=[8,8]) -> None:
-        self.state_size=state_size
-        self.action_size=action_size
+    def __init__(self,discount_factor=0.9,input_size=[8,8]) -> None:
+        self.state_size=STATE_SIZE
         self.discount_factor=discount_factor
         self.buffer=[]
         self.input_size=input_size
         self.main_network=self.build_model()
         self.target_network=self.build_model()
+        self.env=gym.env(ENV)
+        self.action_size=self.env.action_space.n
+        self.eps=0.9
     def build_model(self):
         model = Sequential()
-        model.add(Conv2D(32,input_size=self.input_size, padding='same', input_shape=self.state_size))
-        model.add(MaxPooling2D(2))
-        model.add(Activation('relu'))
+        model.add(ksl.Conv2D(32,input_size=self.input_size, padding='same', input_shape=self.state_size))
+        model.add(ksl.MaxPooling2D(2))
+        model.add(ksl.Activation('relu'))
 
-        model.add(Conv2D(64, strides=2, padding='same'))
-        model.add(MaxPooling2D(2))
-        model.add(Activation('relu'))
+        model.add(ksl.Conv2D(64, strides=2, padding='same'))
+        model.add(ksl.MaxPooling2D(2))
+        model.add(ksl.Activation('relu'))
 
-        model.add(Conv2D(64, (3, 3), strides=1, padding='same'))
-        model.add(MaxPooling2D(2))        
-        model.add(Activation('relu'))
+        model.add(ksl.Conv2D(64, (3, 3), strides=1, padding='same'))
+        model.add(ksl.MaxPooling2D(2))        
+        model.add(ksl.Activation('relu'))
         
-        model.add(Flatten())
+        model.add(ksl.Flatten())
 
-
-        model.add(Dense(512, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
-        # model.compile(loss='mse', optimizer=Adam())
-
+        model.add(ksl.Dense(512, activation='relu'))
+        model.add(ksl.Dense(self.action_size, activation='linear'))
         return model
+    def update_buffer(self, state, action, reward, n_state, is_done):
+        self.buffer.append([state, action, reward, n_state, is_done])
+    def sellect_action(self,Q_value):
+        # epsilone greedy action sellection
+        if np.random.uniform(0,1)>self.eps:
+            return np.random.randint(self.action_size)
+        return np.argmax(Q_value)
     def load_main_network(slef):
         pass
-    def train(self):
+    def train_local_models(self):
+        pass
+    def loss(self):
         pass
     
 
 class agent1(agent):
     def __init__(self) -> None:
-        pass
+        super().__init__()
     def load_main_network(slef):
         pass
     def load_target_network(self):
         pass
+    def train_local_models(self):
+        return 
 
 
 class agent2(agent):
     def __init__(self) -> None:
-        pass
+        super().__init__()
     def load_main_network(slef):
         pass
     def load_target_network(self):
         pass
-
-    
+    def train_local_models(self):
+        return 
---
+++ codes/config.py
@@ -0,0 +1,2 @@
+ENV="MsPacman-v0"
+STATE_SIZE=(88, 80, 1)
