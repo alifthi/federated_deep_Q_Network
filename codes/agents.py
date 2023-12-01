1d9d86ae0a57e667f1f27024bebebf8f83d9c3a9
alifthi <alifathi8008@gmail.com>
2023-11-30 19:19:45 -0600

    Federated_Deep_Q_Network: bug fix

--- codes/agents.py
+++ codes/agents.py
@@ -1,37 +1,38 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='3'
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers as ksl 
 from utils import utils
-from config import ENV, STATE_SIZE, BATCH_SIZE, TARGET_NETWORK_UPDATE_RATE 
+from config import ENV, STATE_SIZE, BATCH_SIZE, TARGET_NETWORK_UPDATE_RATE, DISCOUNT_FACTOR 
 import gym 
 
 class agent:
-    def __init__(self,discount_factor=0.9,input_size=[8,8]) -> None:
+    def __init__(self) -> None:
         self.state_size=STATE_SIZE
         self.batch_size=BATCH_SIZE
-        self.discount_factor=discount_factor
+        self.discount_factor=DISCOUNT_FACTOR
         self.buffer=[]
-        self.input_size=input_size
-        self.main_network=self.build_model()
-        self.target_network=self.build_model()
-        self.env=gym.env(ENV)
+        self.env=gym.make(ENV,render_mode='rgb_array')
         self.action_size=self.env.action_space.n
         self.eps=0.9
-        self.utils=utils
+        self.utils=utils()
         self.target_network_update_rate=TARGET_NETWORK_UPDATE_RATE
         self.total_updates=1
+        self.main_network=self.build_model()
+        self.target_network=self.build_model()
         
     def build_model(self):
         model = Sequential()
-        model.add(ksl.Conv2D(32,input_size=self.input_size, padding='same', input_shape=self.state_size))
+        model.add(ksl.Conv2D(32,kernel_size=3, padding='same', input_shape=self.state_size))
         model.add(ksl.MaxPooling2D(2))
         model.add(ksl.Activation('relu'))
 
-        model.add(ksl.Conv2D(64, strides=2, padding='same'))
+        model.add(ksl.Conv2D(64,kernel_size=3, strides=2, padding='same'))
         model.add(ksl.MaxPooling2D(2))
         model.add(ksl.Activation('relu'))
 
-        model.add(ksl.Conv2D(64, (3, 3), strides=1, padding='same'))
+        model.add(ksl.Conv2D(64, kernel_size=3, strides=1, padding='same'))
         model.add(ksl.MaxPooling2D(2))        
         model.add(ksl.Activation('relu'))
 
@@ -51,16 +52,16 @@ class agent:
     def start_episode(self, time_steps):
         return_=0
         self.update_target_network()
-        state=self.env.reset()
+        state,_=self.env.reset()
         state=self.utils.preprocessing(image=state)
         for _ in range(time_steps):
             self.total_updates+=1
-            self.env.render('rgb_array')
+            self.env.render()
             if self.total_updates%self.target_network_update_rate==0:
                 self.update_target_network()
-            Q_values=self.main_network(state[None,:])
+            Q_values=self.main_network.predict(state[None,:])
             action=self.sellect_action(Q_value=Q_values)
-            n_state, reward, done,_=self.env.step(action)
+            n_state, reward, done,_,_=self.env.step(action)
             n_state=self.utils.preprocessing(n_state)
             self.update_buffer(action=action, reward=reward, n_state=n_state, state=state,is_done=done)
             state=n_state
@@ -81,7 +82,7 @@ class agent:
             self.main_network.fit(state, Q_values, epochs=1)
 
     def update_target_network(self):
-        self.target_network.set_weights(self.main_network.weights())
+        self.target_network.set_weights(self.main_network.weights)
     def loss(self):
         pass
 class agent1(agent):
