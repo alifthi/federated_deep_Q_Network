00a0e06b8a3c630b575f3d6cb3e8570f6d49c43b
alifthi <alifathi8008@gmail.com>
2024-01-21 12:18:40 -0600

    Federated_Deep_Q_Network[VPP]: step method completed

--- codes/vpp_env.py
+++ codes/vpp_env.py
@@ -5,9 +5,7 @@ class vpp_env:
         self.load_datasets('../Dataset/scenario_datasets/')
         self.num_EV_charger=4
         self.action_space=3
-        self.t=0 # second
         self.time_delay=15 # second
-        self.state_in_table=0
         self.naminal_power=3.7
         self.EV_cap=100
         self.time_to_fullcharge=self.EV_cap/self.naminal_power #hours
@@ -23,51 +21,74 @@ class vpp_env:
         self.price=pd.read_csv(path+'market_prices_2020_profile.csv')
         self.houseload_data=pd.read_csv(path+'households_load_profile.csv')
     def step(self,action):
+        episode_done=False
+        rewards=[0,0,0]
         for i in range(self.num_EV):
             if self.parked_time[i]>self.time_to_park[i] or self.soc[i]==1:
+                self.soc_of_departed_EVs.append(self.soc[i])
+                rewards[1]+=self.reward_for_soc(self.soc[i])
                 self.charging_EVs[self.charging_EVs==i]=np.random.sample(range(self.num_EV))
                 self.parked_time[i]=0
                 
         self.t+=self.time_delay
         self.parked_time[self.charging_EVs]=self.parked_time[self.charging_EVs]+self.time_delay/3600
         house_demand=self.houseload_data.iloc[self.state_in_table,1]
-        renewable_energy=self.sp_data.iloc[self.state_in_table,1]+self.wt_data.iloc[self.state_in_table,1]
-        exist_energy=renewable_energy-house_demand                
+        renewable_energy=self.sp_data.iloc[self.state_in_table,1]\
+            +self.wt_data.iloc[self.state_in_table,1]
+        exist_energy=renewable_energy-house_demand          
+        
         self.soc[self.charging_EVs]=self.soc[self.charging_EVs]\
             +(action.T*self.time_delay/(self.time_to_fullcharge*60*60))
+        power=exist_energy+states[0]
         states=[]
         states.append(action.T@(np.ones_like(action)*self.naminal_power))
-        states.append(exist_energy+states[0])
+        states.append(power)
         states.append(self.soc*self.EV_cap)
-        
+        rewards[1]=power
+        self.total_power+=power
+        if power<0:
+            self.purchased_energy+=power
+        else:
+            self.excess_energy+=power
         if not self.t==0 and self.t%900==0:
-            self.t=0
-            self.state_in_table+=1
-            
-        
-    def step_reward_function(self,total_load,soc):
-        rewards=[0,0]
+            rewards[2]=self.trajectory_ending_reward(
+                sum(self.soc_of_departed_EVs)/len(self.soc_of_departed_EVs),
+                                                     self.purchased_energy,
+                                                     self.excess_energy,
+                                                     self.total_power*0.25)
+
+            episode_done=True
+        return states,sum(rewards),episode_done    
+    def reset(self):
+        self.t=0
+        self.state_in_table+=1
+        self.purchased_energy=0
+        self.excess_energy=0
+        self.total_power=0
+        self.soc_of_departed_EVs=[]
+    def reward_for_load_value(self,total_load):
         load_value=self.price.iloc[self.state_in_table,1]*total_load
         if load_value<-1:
-            rewards[0]=load_value+1
+            reward=load_value+1
         elif load_value>=-1 and load_value<0:
-            rewards[0]=15*load_value+15
+            reward=15*load_value+15
         elif load_value>=0 and load_value<1:
-            rewards[0]=-15*load_value+15
+            reward=-15*load_value+15
         else:
-            rewards[0]=-load_value+1
-        
+            reward=-load_value+1
+        return reward
+    def reward_for_soc(self,soc):
         if soc<90:
-            rewards[1]=5*soc-300
+            reward=5*soc-300
         else:
-            rewards[1]=-10*soc+1050
-        return sum(rewards)
+            reward=1050-10*soc
+        return reward
     def trajectory_ending_reward(self,soc,greed_energy,renewable_energy,cost):
         rewards=[0,0,0,0]
         if soc <75:
-            rewards[0]=-9+4*soc/25
+            rewards[0]+=-9+4*soc/25
         else:
-            rewards[0]=9-2*soc/25
+            rewards[0]+=9-2*soc/25
             
         if greed_energy<800:
             rewards[1]=-25*greed_energy+20000
